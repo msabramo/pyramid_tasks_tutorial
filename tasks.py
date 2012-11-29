7d79ed8e3f027da8d561fa9574b5760b8beeb51f
Marc Abramowitz <marca@surveymonkey.com>
2012-11-28 16:19:01 -0800

    Step 3: Set up subscribers for initializing the database and for adding the database to the request

--- tasks.py
+++ tasks.py
@@ -17,15 +17,40 @@ log = logging.getLogger(__file__)
 here = os.path.dirname(os.path.abspath(__file__))
 
 
+@subscriber(ApplicationCreated)
+def application_created_subscriber(event):
+    log.warn('Initializing database...')
+    f = open(os.path.join(here, 'schema.sql'), 'r')
+    stmt = f.read()
+    settings = event.app.registry.settings
+    db = sqlite3.connect(settings['db'])
+    db.executescript(stmt)
+    db.commit()
+    f.close()
+
+@subscriber(NewRequest)
+def new_request_subscriber(event):
+    request = event.request
+    settings = request.registry.settings
+    request.db = sqlite3.connect(settings['db'])
+    request.add_finished_callback(close_db_connection)
+
+def close_db_connection(request):
+    request.db.close()
+
+
 if __name__ == '__main__':
     # configuration settings
     settings = {}
     settings['reload_all'] = True
     settings['debug_all'] = True
+    settings['db'] = os.path.join(here, 'tasks.db')
     # session factory
     session_factory = UnencryptedCookieSessionFactoryConfig('itsaseekreet')
     # configuration setup
     config = Configurator(settings=settings, session_factory=session_factory)
+    # scan for @view_config and @subscriber decorators
+    config.scan()
     # serve app
     app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 8080, app)
