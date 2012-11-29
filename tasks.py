f41d2150524475016cfe481d4bdf84a3ebc56d65
Marc Abramowitz <marca@surveymonkey.com>
2012-11-28 16:05:33 -0800

    Step 2B: Then we’ll set up logging and the current working directory path

--- tasks.py
+++ tasks.py
@@ -5,3 +5,10 @@ from pyramid.config import Configurator
 from pyramid.session import UnencryptedCookieSessionFactoryConfig
 
 from wsgiref.simple_server import make_server
+
+logging.basicConfig()
+log = logging.getLogger(__file__)
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+
